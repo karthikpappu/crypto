360d434c4ae0ad6aba9fb75b1ca702596b260fef
Karthik Pappu <karthik.pappu@gmail.com>
2019-08-13 08:30:20 -0500

    blockchain program

--- blockchain.py
+++ blockchain.py
@@ -3,5 +3,6 @@ blockchain = [1]
 def add_value():
     blockchain.append([blockchain[0],5.3])
     print(blockchain)
+    #test
 
 add_value()                                                                              
