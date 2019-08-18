5a2f42df2d2864c3204f8f1538a97281385f523d
Karthik Pappu <karthik.pappu@gmail.com>
2019-08-18 10:17:25 -0500

    use function and avoid less repeat

--- blockchain.py
+++ blockchain.py
@@ -7,15 +7,21 @@ def get_last_blockchain_value():
 
 def add_value(transaction_amount,last_transaction=[1]):
     blockchain.append([last_transaction,transaction_amount])
-    
 
-tx_amount = float(input('Your transcation amount please: '))
-add_value(tx_amount) 
 
-tx_amount = float(input('Your transcation amount please: '))                                                                           
+def get_user_input():
+    return float(input('Your transcation amount please: '))
+
+
+tx_amount = get_user_input()
+add_value(tx_amount)
+
+
+tx_amount = get_user_input()                                                                           
 add_value(last_transaction=get_last_blockchain_value(),transaction_amount=tx_amount)
 
-tx_amount = float(input('Your transcation amount please: ')) 
+
+tx_amount = get_user_input() 
 add_value(tx_amount,get_last_blockchain_value())
 
 
