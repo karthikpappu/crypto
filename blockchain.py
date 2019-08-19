86c5bc8c52dd0a55bf563695c77938363365c2f1
Karthik Pappu <karthik.pappu@gmail.com>
2019-08-19 08:51:00 -0500

    ifelse - issue check it out

--- blockchain.py
+++ blockchain.py
@@ -12,22 +12,39 @@ def add_value(transaction_amount,last_transaction=[1]):
   blockchain.append([last_transaction,transaction_amount])
 
 
-def get_user_input():
+def get_transcation_value():
     ''' Returns the input of the users ( a new transcation amount) to blockchain'''
-    return float(input('Your transcation amount please: '))
+    user_input = float(input('Your transcation amount please: '))
+    return user_input
 
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
 
-tx_amount = get_user_input()
+def print_blockchain_elements():
+    #output the blockain list to the console
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
+
+#get the first transcation input and add the value to the blockchain
+tx_amount = get_transcation_value()
 add_value(tx_amount)
 
 
 while True:
-    tx_amount = get_user_input() 
-    add_value(tx_amount,get_last_blockchain_value())
+    print('Please choose')
+    print('1: Add a new transcation value')
+    print('2: Output the blockchain blocks')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transcation_value()
+        add_value(tx_amount,get_last_blockchain_value())
+    else:
+        print_blockchain_elements()
 
-    for block in blockchain:
-        print('Outputting Block')
-        print(block)
+
+   
 
 
 print('Done!!!!')
